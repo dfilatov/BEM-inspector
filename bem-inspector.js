a8aa0d4344d1fe757d1adb2c338f506e8256b4d2
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-03-21 14:07:14 +0400

    popup for block, elems, mods added

--- bem-inspector.js
+++ bem-inspector.js
@@ -1,17 +1,31 @@
 (function() {
 
 var curElem,
-    hilightElems = {};
+    hilightElems = {},
+    popupElem = $('<div class="bem-inspector__popup"/>').appendTo('body'),
+    bemRE = /^[bi]\-[a-zA-Z0-9-]+(__[a-zA-Z0-9-]+)?$/;
 
-$.each(['left', 'top', 'right', 'bottom'], function(i, dir) {
-    hilightElems[dir] = $('<div class="__hilighther_' + dir +'"/>').appendTo('body');
+$.each(['left', 'top', 'right', 'bottom'], function(i, border) {
+    hilightElems[border] = $('<div class="bem-inspector__hilighther bem-inspector__hilighther_border_' + border +'"/>')
+        .appendTo('body');
 });
 
-function getClosestBlockOrElem(node) {
+function getNodeBEMs(node) {
 
+    return node.className?
+        node.className.split(' ').filter(function(item) {
+            return bemRE.test(item = item.trim()) && item !== 'i-bem';
+        }) :
+        [];
+
+}
+
+function getClosestBEMs(node) {
+
+    var bems;
     do {
-        if(node.className && node.className.indexOf('b-') > -1) {
-            return node;
+        if((bems = getNodeBEMs(node)).length) {
+            return { node : $(node), bems : bems };
         }
     }
     while(node = node.parentNode);
@@ -20,14 +34,36 @@ function getClosestBlockOrElem(node) {
 
 }
 
-function hilight(elem) {
+function bemIsElem(bem) {
+    return bem.indexOf('__') > -1;
+}
 
-    var offset = elem.offset(),
+function extractMods(node, bem) {
+
+    var res;
+
+    node.className && node.className.split(' ').forEach(function(item) {
+        var hasMod = (item = item.trim()).indexOf(bem + '_') === 0 && item.indexOf(bem + '__') === -1;
+        if(hasMod) {
+            var modNameIdx = bem.length + 1,
+                modValIdx = item.indexOf('_', modNameIdx) + 1;
+            modValIdx &&
+                ((res || (res = {}))[item.substr(modNameIdx, modValIdx - modNameIdx - 1)] = item.substr(modValIdx));
+        }
+    });
+
+    return res;
+
+}
+
+function hilight(bems) {
+
+    var offset = bems.node.offset(),
         pos = {
             'left'   : offset.left,
             'top'    : offset.top,
-            'width'  : curElem.outerWidth(),
-            'height' : curElem.outerHeight()
+            'width'  : bems.node.outerWidth(),
+            'height' : bems.node.outerHeight()
         };
 
     hilightElems.left
@@ -43,6 +79,31 @@ function hilight(elem) {
         .css({ left : pos.left, top : pos.top + pos.height, width : pos.width })
         .show();
 
+    popupElem
+        .html(bems.bems.map(function(bem) {
+            var mods = extractMods(bems.node[0], bem),
+                res = ['<div class="bem-inspector__bem'];
+
+            bemIsElem(bem) && res.push(' bem-inspector__bem_type_elem');
+            res.push('"><div class="bem-inspector__bem-title">', bem, '</div>');
+
+            if(mods) {
+                res.push('<table class="bem-inspector__mods">');
+                $.each(mods, function(modName, modVal) {
+                    res.push('<tr>');
+                    res.push('<td class="bem-inspector__bem-mod-name">', modName, '</td>');
+                    res.push('<td class="bem-inspector__bem-mod-val">', modVal, '</td>');
+                    res.push('</tr>');
+                });
+                res.push('</table>')
+            }
+
+            res.push('</div>');
+            return res.join('');
+        }).join(''))
+        .css({ left : pos.left, top : pos.top + pos.height + 1 })
+        .show();
+
 }
 
 function unhilight() {
@@ -56,12 +117,12 @@ function unhilight() {
 $('body').on(
     {
         'mouseenter' : function(e) {
-            var blockOrElem = getClosestBlockOrElem(e.target);
-            if(!blockOrElem) {
+            var bems = getClosestBEMs(e.target);
+            if(!bems) {
                 return;
             }
 
-            hilight(curElem = $(blockOrElem));
+            hilight(bems);
         },
         'mouseleave' : function() {
             if(curElem) {
