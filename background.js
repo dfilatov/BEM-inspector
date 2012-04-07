117ab4e262f53b0f83b77b3a928a243c8ee1b917
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-04-07 11:31:10 +0400

    raw bem tree

--- background.js
+++ background.js
@@ -1,14 +1,12 @@
 chrome.extension.onConnect.addListener(function(portForDevTools) {
-    console.log('back connnected');
     chrome.tabs.getSelected(null, function(tab) {
-        console.log('on get selectted');
         var portForContent = chrome.tabs.connect(tab.id);
         portForDevTools.onMessage.addListener(function(msg) {
-            console.log('from dev', msg)
+            console.log('from dev', msg);
             portForContent.postMessage(msg);
         });
         portForContent.onMessage.addListener(function(msg) {
-            console.log('from content', msg)
+            console.log('from content', msg);
             portForDevTools.postMessage(msg);
         });
     });
